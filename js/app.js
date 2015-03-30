35c73d1b6c0a56b924911a33c2c5f2b13b22568d
Li Hsuan Chen <vincent@vincenttt.com>
2015-03-30 17:26:16 +0800

    內容以 angularjs render

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var klifeApp = angular.module('klifeApp', []);
+
+klifeApp.controller('BuildListController', function ($scope, $http) {
+	$scope.builds = [{
+		"date": {
+			"year": "2015",
+			"month": "03",
+			"day": "30"
+		},
+		"version": "0.1.1",
+		"chanegeLog": "",
+		"plistID": "xip9s6kelulvuhu",
+		"ipaID": "hyrapx6gypzi9zf",
+	}];
+});
