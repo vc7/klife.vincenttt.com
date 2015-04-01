852d95e5fdfd4f5a9036e6f5040e3f28f89d54fc
Li Hsuan Chen <vincent@vincenttt.com>
2015-04-01 18:42:01 +0800

    Add build

--- js/app.js
+++ js/app.js
@@ -19,5 +19,16 @@ klifeApp.controller('BuildListController', function ($scope, $http) {
 		"chanegeLog": "",
 		"plistID": "knuf1l8o4q1l360",
 		"ipaID": "hyrapx6gypzi9zf",
+	},
+	{
+		"date": {
+			"year": "2015",
+			"month": "04",
+			"day": "01"
+		},
+		"version": "0.1.2",
+		"chanegeLog": "(1) 整併內容簡介和目錄 (2) 新增評論呈現的 UI (3) 加入評等的 UI 及功能， server 資料待接",
+		"plistID": "qy6z4tk76y5njen",
+		"ipaID": "kp62qmeal9zjstz",
 	}];
 });
